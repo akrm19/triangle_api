89b47913acad24b47960980a2e6f38d683e12846
akrm19 <akrm19@gmail.com>
2020-01-23 10:25:15 -0600

    Add empty triangle api controller & initial coordinate model

---
+++ Controllers/TrianglesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TriangleApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TriangleApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TrianglesController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public ActionResult<string> Get([FromQuery]TriangleCoordinate coordinate)
+        {
+            try
+            {
+                var temp = $"coodinate: col:{coordinate.Column}, row:{coordinate.Row}";
+                return Ok(temp);
+            }
+            catch(Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Unexpected error occured: {e.Message}");
+            }
+        }
+    }
+}
---
+++ Models/TriangleCoordinate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TriangleApi.Models
+{
+    public class TriangleCoordinate
+    {
+        public char Row { get; set; }
+
+        public int Column { get; set; }
+
+        public TriangleCoordinate()
+        {
+
+        }
+    }
+}
